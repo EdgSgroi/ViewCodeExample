761a56655ebd5cb92b687f57a515d54f6b47cbfa
EdgSgroi <edgar.sgroi@gmail.com>
2020-09-11 17:46:33 -0300

    Organize the comments

--- ViewCodeExample/ViewCodeExample/ViewController.swift
+++ ViewCodeExample/ViewCodeExample/ViewController.swift
@@ -27,11 +27,11 @@ extension CodeView {
 }
 
 
-/* Property Wrapper criado para encapsular uma propriedade que irá se repetir */
+// Property Wrapper criado para encapsular uma propriedade que irá se repetir
 @propertyWrapper class AutoLayout<T: UIView> {
     private lazy var view: T = {
         let view = T(frame: .zero)
-        /* False pois nós queremos definir os frames da table view com constraints, e não que isso seja feito de forma automática */
+        // O valor "false" foi atribuido pois nós queremos definir os frames da table view com constraints, e não que isso seja feito de forma automática
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = self.backgroundColor
         
@@ -65,7 +65,7 @@ class ViewController: UIViewController, CodeView {
     }
     
     func addViewToHierarchy() {
-        /* Irá adicionar a table view como uma subview da view do viewcontroller. Isso é necessário para que ela seja exibida */
+        // Irá adicionar a table view como uma subview da view do viewcontroller. Isso é necessário para que ela seja exibida
         self.view.addSubview(tableView)
     }
     
@@ -74,10 +74,10 @@ class ViewController: UIViewController, CodeView {
             !-> Um detalhe importante é que as constraints só podem ser configuradas após a view ser adicionada na view principal. Não tem como confirar as contraints de uma view que não existe na tela.
          */
         NSLayoutConstraint.activate([
-            /*
-             view.safeAreaLayoutGuide: respeitará as safe areas apenas do top e bottom
-             view.layoutMarginsGuide: respeitará todas as safe areas, incluse as laterais
-             */
+            
+             // view.safeAreaLayoutGuide: respeitará as safe areas apenas do top e bottom
+             // view.layoutMarginsGuide: respeitará todas as safe areas, incluse as laterais
+             
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
             tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
